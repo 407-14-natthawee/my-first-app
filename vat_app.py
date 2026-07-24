ee203c09560453e0c5e5d0df0f568c4f0f46ba6e
407-14-natthawee <54232@yupparaj.ac.th>
2026-07-24 11:30:53 +0700

    Create vat_app.py

---
+++ vat_app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+st.title("🛒แอปพลิเคชั่นคำนวณราคาสินค้ารวม VAT 7%")
+
+price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
+st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
+
+net_price = price - vat
+vat = price * 0.07
+st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
+
+st.divider()
+
+st.write("นางสาวณัฎฐวี คำลือ เลขที่ 14  ม.4/7")
